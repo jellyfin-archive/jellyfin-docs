ea1da3fc8be9088bcb68a3387829d7368201290d
dkanada <dkanada@users.noreply.github.com>
2020-04-19 12:21:24 +0900

    fix conditions and update docfx command

--- .ci/build-in-ci.sh
+++ .ci/build-in-ci.sh
@@ -1,3 +1,3 @@
 wget https://github.com/dotnet/docfx/releases/latest/download/docfx.zip
 unzip docfx.zip -d docfx-cli
-mono docfx-cli/docfx.exe --warningsAsErrors
+mono docfx-cli/docfx.exe --warningsAsErrors docfx.json

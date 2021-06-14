85a089eaaa9dc3e8f8f9fd6127e75e1ae7b0833b
Anthony Lavado <anthony@lavado.ca>
2021-06-14 15:17:14 -0400

    Revert back to correct package

--- .ci/build-in-ci.ps1
+++ .ci/build-in-ci.ps1
@@ -1,5 +1,5 @@
-nuget install docfx
-docfx --warningsAsErrors docfx.json
+nuget install docfx.console -Verbosity detailed
+docfx.exe --warningsAsErrors docfx.json
 if ($lastexitcode -ne 0){
     throw ("Error generating document")
 }

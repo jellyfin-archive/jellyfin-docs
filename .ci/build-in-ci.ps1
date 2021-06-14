bb9f061bbaa18218d36f118d86cf959a15b89f35
Anthony Lavado <anthony@lavado.ca>
2021-06-14 15:29:49 -0400

    Update build-in-ci.ps1

--- .ci/build-in-ci.ps1
+++ .ci/build-in-ci.ps1
@@ -1,6 +1,5 @@
 nuget install docfx.console
-cd .\docfx.console*\tools\
-.\docfx.exe --warningsAsErrors docfx.json
+.\docfx.console*\tools\docfx.exe --warningsAsErrors docfx.json
 if ($lastexitcode -ne 0){
     throw ("Error generating document")
 }
